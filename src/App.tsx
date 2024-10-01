be715df3763b6f0b61a52153bba6dd2125441585
shmuelmori <shmuelmuri@gmail.com>
2024-10-01 12:17:49 +0300

    hello man

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,19 @@ import Layout from './Layout/Layout'
 function App() {
 
   return (
-   <>
-   <BrowserRouter>
-   <Layout/>
-   <Routes>
-    <Route path='/users' />
-    <Route path='/about' />
-    <Route path='/services' />
-    <Route path='/contact' />
-    <Route path='/profile' />
-    <Route path='/dev' />
-   </Routes>
-   </BrowserRouter>
-   </>
+    <>
+      <BrowserRouter>
+        <Layout />
+        <Routes>
+          <Route path='/users' />
+          <Route path='/about' />
+          <Route path='/services' />
+          <Route path='/contact' />
+          <Route path='/profile' />
+          <Route path='/dev' />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
