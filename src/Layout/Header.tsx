be5b339b001e45216413f219881166170b567aad
david <025860ds@gmail.com>
2024-10-08 09:44:10 +0300

    finish

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -8,11 +8,10 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ toggleAside, isAsideOpen }: HeaderProps) => {
 
   return (
-    <header className="bg-gray-800 text-white flex justify-end items-center fixed top-0 w-full shadow-md">
-      <button 
-        onClick={toggleAside} 
-        className={`flex items-center justify-center w-12 h-8 border-2 border-gray-400 rounded-lg hover:bg-slate-300 m-2 cursor-pointer transition-transform duration-500 ease-in-out ${isAsideOpen ? 'mr-[170px]' : 'mr-5'}`}
-      >
+    <header className="bg-gray-800 text-white flex justify-end fixed top-0 w-full shadow-md">
+      <button
+        onClick={toggleAside}
+        className={`flex items-center justify-center w-12 h-8 border-2 border-gray-400 rounded-lg hover:bg-slate-300 m-2 cursor-pointer transition-transform duration-500 ease-in-out ${isAsideOpen ? 'mr-[170px]' : 'mr-5'}`}>
         <LuMenu className='w-12 h-6 text-gray-600' />
       </button>
     </header>
