08b13709c6ef80ef3517bacc3c223eea048dee7b
shmuelmori <shmuelmuri@gmail.com>
2024-10-21 13:56:55 +0300

    createTask-ux/ui and intgrtion

--- src/hooks/useTask.tsx
+++ src/hooks/useTask.tsx
@@ -1,12 +1,38 @@
 import axios from 'axios';
 import { errorFromServer, successFromServer } from '../utils/toast';
-import { ITask } from '../utils/types';
+import { AppDispatch } from '../redux/store';
+import { useDispatch } from 'react-redux';
+import { addTask } from '../redux/slices/taskSlice';
 
-const BASEURL = "http://localhost:3001/task/";
 
-export default function useGroup() {
+type taskType = {
+    name: string;
+    description: string;
+    status: "TO DO" | "IN PROGRESS" | "COMPLETE";
+    duration: number;
+    groupId: string;
+};
 
-    return {}
+export default function useTask() {
+    const BASEURL = "http://localhost:3001/task/";
+    const dispatch: AppDispatch = useDispatch();
+
+
+    const createTask = async (newTask: taskType) => {
+        try {
+            const response = await axios.post(`${BASEURL}createTask`, newTask, { withCredentials: true })
+            if (response.data.isSuccessful) {
+                dispatch(addTask(response.data.data));
+                successFromServer(response.data.displayMessage)
+            }
+        }
+        catch (err) {
+            if (axios.isAxiosError(err))
+                errorFromServer(err.response?.data.displayMessage)
+        }
+    }
+
+    return { createTask }
 }
 
 
--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { HiPlusSm } from "react-icons/hi";
 import TableTask from "../components/TableTask";
 import { initialTasks } from "../redux/slices/taskSlice";
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from "../redux/store";
+import CreateTask from "../components/CreateTask";
 
 export default function TaskPage() {
     const dispatch = useDispatch<AppDispatch>();
     const { id } = useParams<{ id: string }>();
     const tasks = useSelector((state: RootState) => state.tasks.tasks);
+    const [popCreateTask, setPopCreateTask] = useState<boolean>(false);
 
     useEffect(() => {
         dispatch(initialTasks(id!));
@@ -19,10 +22,16 @@ export default function TaskPage() {
             <div className="flex w-[80%] justify-center p-4 text-2xl font-bold text-gray-600 dark:text-white">{tasks.length} Tasks active</div>
 
             <div className="w-[80%] flex items-center">
-                <div className="flex space-x-4 ml-2"></div>
+                <button
+                    className="flex items-center"
+                    onClick={() => {
+                        setPopCreateTask(!popCreateTask);
+                    }}><HiPlusSm /> Add task</button>
             </div>
 
             {tasks.length > 0 && <TableTask tasks={tasks} />}
+
+            {popCreateTask && <CreateTask groupId={id!} setPopCreateTask={setPopCreateTask} />}
         </div>
     )
 }
